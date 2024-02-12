7323213e0e1782d86b5db7a88f744f090e7bc31c
Evan Ernst <50885631+evanernst00@users.noreply.github.com>
2024-02-12 08:49:13 -0700

    Created classes example

---
+++ C++/classes/Cylinder.h
@@ -0,0 +1,15 @@
+#ifndef CYLINDER_H
+#define CYLINDER_H
+
+const float PI = 3.14159;
+
+class Cylinder
+{
+    public:
+        double height;
+        double radius;
+
+        double volume();
+};
+
+#endif //CYLINDER_H
