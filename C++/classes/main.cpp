e6e44238d93dd8650cea7b41f2bdcf41fdfd8b41
Evan Ernst <50885631+evanernst00@users.noreply.github.com>
2024-02-12 17:30:09 -0700

    Finished cpp classes experimentation

--- C++/classes/main.cpp
+++ C++/classes/main.cpp
@@ -9,5 +9,7 @@ int main()
     can1.height = 4.5;
     can1.radius = 1.25;
 
-    
+    cout << "Height: " << can1.height << endl;
+    cout << "Radius: " << can1.radius << endl;
+    cout << "Volume: " << can1.volume() << endl;
 }
