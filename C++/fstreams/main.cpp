6aa3338f3c8aefdc440f3e3935c185e1a3f21414
Evan Ernst <50885631+evanernst00@users.noreply.github.com>
2024-02-16 12:20:48 -0700

    fstreams started

--- C++/fstreams/main.cpp
+++ C++/fstreams/main.cpp
@@ -5,12 +5,39 @@ using namespace std;
 
 int main()
 {
-    // Inputting from a file
-    string fileData;
-    ifstream myInput("pets.txt");
-    myInput >> fileData;
+    // --- Inputting from a file ---
+    string fileReader; // Normal variable
 
-    cout << fileData;
+    ifstream pets("pets.txt");
+
+    // Check for error
+    if(pets.fail()) // can also do !pets or !pets.is_open()
+    {
+        // streams: cin, cout, cerr
+        cerr << "Failed to open file";
+        // return failure
+        return -1;
+    }
+
+    // Separated by whitespace
+    while(pets >> fileReader)
+    {
+        cout << fileReader << " ";
+    }
+    cout << endl;
+
+    pets.close();
+
+    // --- Outputting to a file ---
+    ofstream petsOut("pets.txt");
+
+    string newPet;
+    cout << "Add a pet to the file: ";
+    cin >> newPet;
+    petsOut << newPet;
+
+    // Make sure to close the file
+    petsOut.close();
 
     return 0;
 }
