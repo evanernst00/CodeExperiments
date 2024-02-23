b66d39314839c567cae27bac275686d19092212c
Evan Ernst <50885631+evanernst00@users.noreply.github.com>
2024-02-23 08:31:51 -0700

    learned more cpp classes

--- C++/classes/Cylinder.h
+++ C++/classes/Cylinder.h
@@ -4,15 +4,17 @@
 class Cylinder
 {
     public:
-        double height;
-        double radius;
-
+        Cylinder();
+        Cylinder(const double h, const double r);
         /**
          * @brief calculate volume
          * 
          * @return double - the volume of the cylinder
          */
         double volume();
+    private:
+        double height;
+        double radius;
 };
 
 #endif //CYLINDER_H
