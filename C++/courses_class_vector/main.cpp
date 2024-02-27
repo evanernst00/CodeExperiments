fc383f0672e4ffea44e71f0c3bc57f1d8f15ddca
Evan Ernst <50885631+evanernst00@users.noreply.github.com>
2024-02-26 22:45:49 -0700

    Wrote main to demonstrate class

--- C++/courses_class_vector/main.cpp
+++ C++/courses_class_vector/main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <vector>
 #include <string>
 #include "Course.hpp"
@@ -5,9 +6,23 @@ using namespace std;
 
 int main()
 {
-    vector<Course>(Courses);
+    vector<Course*> Courses;
 
-    
+    Courses.push_back(new Course("CSCI200"));
+    Courses.push_back(new Course("MATH121"));
+    Courses.push_back(new Course("CSCI220"));
+    Courses[0]->registerStudent();
+    Courses[0]->registerStudent();
+    Courses[1]->registerStudent();
+    Courses[2]->registerStudent();
+    Courses[2]->registerStudent();
+    Courses[2]->withdrawStudent();
+
+    for(size_t i = 0; i < Courses.size(); i++)
+    {
+        cout << "Course: " << Courses[i]->getTitle() 
+        << " Enrollment: " << Courses[i]->getEnrollment() << endl;
+    }
 
     return 0;
 }
